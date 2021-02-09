5fc033b66039ad6409adc6dfe4bf7d40765cc12c
Adrianne Castro <adrianneccastro@gmail.com>
2021-02-08 22:28:13 -0300

    day 3

---
+++ scripts.js
@@ -0,0 +1,84 @@
+const Modal = {
+  open() {
+    document.querySelector(".modal-overlay").classList.add("active");
+  },
+  close() {
+    document.querySelector(".modal-overlay").classList.remove("active");
+  },
+};
+
+const transactions = [
+  { 
+    id: 1,
+    description: 'Light',
+    amount: -50000,
+    date: '23/01/2021',
+  },
+  { 
+    id: 2,
+    description: 'Website',
+    amount: 500000,
+    date: '23/01/2021',
+  },
+  { 
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021',
+  },
+  { 
+    id: 4,
+    description: 'App',
+    amount: 200000,
+    date: '23/01/2021',
+  },
+]
+
+const Transaction = {
+  incomes() {
+      // add the inputs
+  },
+  expenses() {
+      // add the outputs
+  },
+  total() {
+      // inputs - outputs 
+  }
+}
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income":
+    "expense"
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+    <td class="description">${transaction.description}</td>
+    <td class=${CSSclass}>${transaction.amount}</td>
+    <td class="date">${transaction.date}</td>
+    <td>
+        <img src="./assets/minus.svg" alt="Remove transaction">
+    </td>
+    `
+    return html
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : ""
+  }
+}
+
+transactions.forEach(function(transaction){
+  DOM.addTransaction(transaction)
+})
